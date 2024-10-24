75deae16891414c43b40c17d3c11c7cb9e7a32db
FurryBasket9978 <pgillbama@icloud.com>
2024-10-24 11:02:18 -0500

    added testing capabilities to the board class

--- board.py
+++ board.py
@@ -9,8 +9,23 @@ class Board:
         self.surface.fill(WHITE)
         for i in range(10):
             if i % 3 == 0:
-                pygame.draw.rect(self.surface, BLACK, pygame.Rect((i*(WIDTH/9), 0), (10, 800)))
-                pygame.draw.rect(self.surface, BLACK, pygame.Rect((0, i*(HEIGHT/9)), (800, 10)))
+                pygame.draw.rect(self.surface, BLACK, pygame.Rect(i*(WIDTH/9), 0, 10, 800))
+                pygame.draw.rect(self.surface, BLACK, pygame.Rect(0, i*(HEIGHT/9), 800, 10))
             else:
-                pygame.draw.rect(self.surface, BLACK, pygame.Rect((i*(WIDTH/9), 0)), (5, 800))
-                pygame.draw.rect(self.surface, BLACK, pygame.Rect((0, (i*(HEIGHT/9))), (800, 5)))
+                pygame.draw.rect(self.surface, BLACK, pygame.Rect(i*(WIDTH/9), 0, 5, 800))
+                pygame.draw.rect(self.surface, BLACK, pygame.Rect(0, (i*(HEIGHT/9)), 800, 5))
+
+
+if __name__ == "__main__":
+    surface = pygame.display.set_mode((WIDTH, HEIGHT)) 
+    running = True
+    board = Board(surface) 
+    board.drawBoard()
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+        pygame.display.update()
