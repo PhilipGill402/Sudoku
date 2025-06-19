fec9a31e9db60ee7c445240491cb96ec33be48f6
Philip Gill <48932124+PhilipGill402@users.noreply.github.com>
2025-06-19 00:43:06 -0500

    Create cell.py

---
+++ cell.py
@@ -0,0 +1,36 @@
+class Cell:
+    def __init__(self, value=0):
+        self.options = [1,2,3,4,5,6,7,8,9]
+        self.collapsed = False
+        self.value = value
+        if self.value != 0:
+            self.options = [self.value]
+
+    def isValid(self, board, num, pos):
+        row = board[pos[1]]
+        col = []
+        box = []
+        box.append(pos[0]//3)
+        box.append(pos[1]//3)
+
+        for i in range(box[0] * 3, box[0] * 3 + 3):
+            for j in range(box[1] * 3, box[1] * 3 + 3):
+                if board[i][j].value == num:
+                    return False
+
+        for i in board:
+            col.append(i[pos[0]])
+
+        for i in row:
+            if i.value == num:
+                return False
+        for i in col:
+            if i.value == num:
+                return False
+        return True
+
+    def reduceEntropy(self, board, pos):
+        self.options = []
+        for i in range(1,10):
+            if self.isValid(board, i, pos):
+                self.options.append(i)
