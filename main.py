a055c5400e594f2aa4d0d08ee908eee021547287
Philip Gill <48932124+PhilipGill402@users.noreply.github.com>
2025-06-19 00:39:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from game import *
 
 pygame.init()
 
-surface = pygame.display.set_mode((REAL_WIDTH, REAL_HEIGHT))
+surface = pygame.display.set_mode((WIDTH, HEIGHT))
 running = True
 GameBoard = Board(surface)
 game = Game(surface)
-board = GameBoard.board 
+board = GameBoard.board
+
 
 while running:
     
@@ -19,11 +20,31 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONUP:
             pos = game.getPos(pygame.mouse.get_pos())
-            game.selectPos(pos, board) 
-            print(pos);
+            game.selectPos(pos, board)
 
-    if game.selected: 
-        game.highlightSelected()
+        elif event.type == pygame.KEYDOWN and game.selected is not None:
+            x, y = game.getPos(pygame.mouse.get_pos())
+            if event.key == pygame.K_1:
+                board[y][x] = Cell(1)
+            elif event.key == pygame.K_2:
+                board[y][x] = Cell(2)
+            elif event.key == pygame.K_3:
+                board[y][x] = Cell(3)
+            elif event.key == pygame.K_4:
+                board[y][x] = Cell(4)
+            elif event.key == pygame.K_5:
+                board[y][x] = Cell(5)
+            elif event.key == pygame.K_6:
+                board[y][x] = Cell(6)
+            elif event.key == pygame.K_7:
+                board[y][x] = Cell(7)
+            elif event.key == pygame.K_8:
+                board[y][x] = Cell(8)
+            elif event.key == pygame.K_9:
+                board[y][x] = Cell(9)
+
+    if game.selected is not None: 
+        game.highlightSelected(pos, board)
     pygame.display.flip()
     pygame.display.update()
 
